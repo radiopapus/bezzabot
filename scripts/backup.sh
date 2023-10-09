b15efb4f24b9118eb7a11844286206be13e85d6c
Radiopapus <viktor@zharina.info>
2023-10-09 18:19:56 +0200

    test github action

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,24 +1,10 @@
-#
-# ______          _ _
-# | ___ \        | (_)
-# | |_/ /__ _  __| |_  ___  _ __   __ _ _ __  _   _ ___
-# |    // _` |/ _` | |/ _ \| '_ \ / _` | '_ \| | | / __|
-# | |\ \ (_| | (_| | | (_) | |_) | (_| | |_) | |_| \__ \
-# \_| \_\__,_|\__,_|_|\___/| .__/ \__,_| .__/ \__,_|___/
-#                          | |         | |
-#                          |_|         |_|
-#
-# twitch: twitch.tv/radiopapus
-# github: https://github.com/radiopapus
-# telegram: https://t.me/radiopapus
-#
-# Отказ от ответственности - Использовать только в образовательных целях. Распространяется "как есть".
-#
-# Disclaimer - Only for educational purposes.
-#
-# 2023.
-#
-#
+#!/bin/bash
 
-# sshrasp is an alias for ssh connection.
-sshrasp "sudo -S dd if=/dev/mmcblk0 bs=8M status=progress | gzip -1 -" | sudo dd of=bezzabot_image.gz bs=8M
+set -o errexit -o noclobber -o nounset -o pipefail
+
+BOT_NAME="bezzabot"
+ABS_BEZZABOT_PATH="/srv/$BOT_NAME"
+BEZZABOT_IP="192.168.1.83"
+BEZZABOT_SSH_PORT=2828
+
+ssh -p $BEZZABOT_SSH_PORT radio@$BEZZABOT_IP "sudo dd if=/dev/mmcblk0 bs=100M status=progress | gzip -1 -" | sudo dd of=bezzabot_image.gz bs=100M
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,19 @@ ABS_BEZZABOT_PATH="/srv/$BOT_NAME"
 BEZZABOT_IP="192.168.1.83"
 BEZZABOT_SSH_PORT=2828
 
+echo "Stop bezzabot service"
+ssh -p $BEZZABOT_SSH_PORT radio@$BEZZABOT_IP "systemctl --user stop bezzabot.service"
+
 echo "Backup previous version"
 ssh -p $BEZZABOT_SSH_PORT radio@$BEZZABOT_IP "cp $ABS_BEZZABOT_PATH/$BOT_NAME $ABS_BEZZABOT_PATH/$BOT_NAME.backup"
 
 echo "Deploy"
 scp -P $BEZZABOT_SSH_PORT target/arm-unknown-linux-musleabihf/release/$BOT_NAME radio@$BEZZABOT_IP:$ABS_BEZZABOT_PATH
+
+echo "Start bezzabot service"
+ssh -p $BEZZABOT_SSH_PORT radio@$BEZZABOT_IP "systemctl --user start bezzabot.service"
+
+echo "Check bezzabot service"
+ssh -p $BEZZABOT_SSH_PORT radio@$BEZZABOT_IP "systemctl --user status bezzabot.service"
 
 echo "Finished"
